986881a6e21cdd28b9db1239f8c7573baa487954
kelvin kanu <kelvinkanu2@gmail.com>
2025-10-31 09:39:27 +0100

    Add digital clock app source files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DigitalClock from "./Components/DigitalClock";
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+      <div>
+        <Navbar/>
+        <DigitalClock/>
+      </div>
+  );
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='word_para'>
+        <h1>KelPacks Digital Clock</h1>
+    </div>
+  )
+}
+
+export default Navbar
